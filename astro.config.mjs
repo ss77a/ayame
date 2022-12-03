a82a0bc3cbd8f6162ef5529a7d4f2ff7af71c383
Sam <ss7i@outlook.com>
2022-12-03 13:07:37 +0000

    formating in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,54 +10,64 @@ import { remarkReadingTime } from './src/utils/frontmatter.mjs';
 import Inspect from 'vite-plugin-inspect';
 import { SITE } from './src/config.mjs';
 import critters from 'astro-critters';
-import compress from "astro-compress";
+import compress from 'astro-compress';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const whenExternalScripts = (items = []) => SITE.googleAnalyticsId ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
+const whenExternalScripts = (items = []) =>
+	SITE.googleAnalyticsId ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
-  site: SITE.origin,
-  base: SITE.basePathname,
-  trailingSlash: SITE.trailingSlash ? 'always' : 'never',
-  output: 'static',
-  integrations: [tailwind({
-    config: {
-      path: './tailwind.config.cjs',
-      applyBaseStyles: false
-    }
-  }), sitemap(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), mdx(), /* Disable this integration if you don't use Google Analytics (or other external script). */
-  ...whenExternalScripts(() => partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  })), critters({
-    path: './dist',
-    logger: 'debug',
-    logLevel: 'debug',
-    compress: true,
-    pruneSource: true,
-    preload: 'swap',
-    inlineFonts: true,
-    keyframes: 'critical'
-  }), compress()],
-  /* this is an extension of mdx - a message says that the mdx call should be removed it causes issues. as does removing the extension - images wont build from blog files */
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-    extendDefaultPlugins: true
-  },
-  vite: {
-    plugins: [Inspect()],
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, './src'),
-        '@src': path.resolve(__dirname, 'src/'),
-        '@pkg': path.resolve(__dirname, './node_modules')
-      }
-    }
-  }
+	// Astro uses this full URL to generate your sitemap and canonical URLs in your final build
+	site: SITE.origin,
+	base: SITE.basePathname,
+	trailingSlash: SITE.trailingSlash ? 'always' : 'never',
+	output: 'static',
+	integrations: [
+		tailwind({
+			config: {
+				path: './tailwind.config.cjs',
+				applyBaseStyles: false,
+			},
+		}),
+		sitemap(),
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp',
+		}),
+		mdx() /* Disable this integration if you don't use Google Analytics (or other external script). */,
+		...whenExternalScripts(() =>
+			partytown({
+				config: {
+					forward: ['dataLayer.push'],
+				},
+			})
+		),
+		critters({
+			path: './dist',
+			logger: 'debug',
+			logLevel: 'debug',
+			compress: true,
+			pruneSource: true,
+			preload: 'swap',
+			inlineFonts: true,
+			keyframes: 'critical',
+		}),
+		compress(),
+	],
+	/* this is an extension of mdx - a message says that the mdx call should be removed it causes issues. as does removing the extension - images wont build from blog files */
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		extendDefaultPlugins: true,
+	},
+	vite: {
+		plugins: [Inspect()],
+		resolve: {
+			alias: {
+				'~': path.resolve(__dirname, './src'),
+				'@src': path.resolve(__dirname, 'src/'),
+				'@pkg': path.resolve(__dirname, './node_modules'),
+			},
+		},
+	},
 });
