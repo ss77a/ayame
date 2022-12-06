df25b7a1356e7f7f71f87e74e9d952a61cdee9d5
Sam <ss7i@outlook.com>
2022-12-06 22:53:00 +0000

    formating in favicon but this might need replacing if setting cant be  tweaked for more file putput

--- favicon.js
+++ favicon.js
@@ -1,7 +1,7 @@
 const generateFavicons = require('simple-favicon-generator');
 
 (async () => {
-  const targetImage = './src/assets/images/demo/logo.png';
-  const siteName = 'Ayame 6';
-  await generateFavicons(targetImage, siteName, './public/');
+	const targetImage = './src/assets/images/demo/logo.png';
+	const siteName = 'Ayame 6';
+	await generateFavicons(targetImage, siteName, './public/');
 })();
